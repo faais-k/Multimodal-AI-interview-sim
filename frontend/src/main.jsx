d2b1be7f0cb1e8da6f35a6544801a1f41e36edbc
Faais Bin Kasim <faaisbinkasim@gmail.com>
2026-04-25 11:14:32 +0530

    root-fix: promote interview to context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
+import { InterviewProvider } from "./contexts/InterviewContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <InterviewProvider>
+        <App />
+      </InterviewProvider>
     </AuthProvider>
   </React.StrictMode>
 );
